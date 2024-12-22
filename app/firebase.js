9f6a7fd9e8064fdcbc8c66bada2e07cc271c0552
Tomohiro-Taniguchi <m241731@hiroshima-u.ac.jp>
2024-12-22 20:41:52 +0900

    firebaseの初期設定

---
+++ app/firebase.js
@@ -0,0 +1,18 @@
+// 必要な関数を import
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyALcWHaItRKxAx1nZDE1G6F-uvNNKwpdU8",
+  authDomain: "electricals-original-team-app.firebaseapp.com",
+  projectId: "electricals-original-team-app",
+  storageBucket: "electricals-original-team-app.firebasestorage.app",
+  messagingSenderId: "53779975097",
+  appId: "1:53779975097:web:000d8b3e9e0def88800468",
+  measurementId: "G-ZNXWVM3XP0",
+};
+
+// Firebaseアプリオブジェクトを初期化
+const app = initializeApp(firebaseConfig);
+// Firestoreを読み込み、db(databaseの略)として export
+export const db = getFirestore(app);
